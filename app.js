5185ff8f6a1fafe4768f1e2ef2e9c0e07a5ca380
Cesar Pizarra <111284169+cesarpizarra@users.noreply.github.com>
2026-02-03 22:03:31 +0800

    refactor: restructure project as expressjs starter kit

--- app.js
+++ app.js
@@ -1,1 +1,10 @@
-console.log('Hello world');
+const express = require('express');
+const app = express();
+
+app.get('/', (req, res) => {
+  res.json({ message: 'Hello World' });
+});
+
+app.listen(3000, () => {
+  console.log('Server is running on port 3000');
+});
